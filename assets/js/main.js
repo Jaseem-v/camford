85515cff2076576fdcf627dbdcabc150c0f67bd1
Jaseem-v <muhammedjaseem753@gmail.com>
2021-08-12 15:44:30 +0100

    errors fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@ var swiper = new Swiper(".expert-swiper", {
             slidesPerView: 2,
             spaceBetween: 10,
         },
-       
+
     },
     autoplay: {
         delay: 2500,
@@ -160,3 +160,44 @@ $(window).on('scroll', function () {
     }
 
 })
+
+
+/////////////////////////////////////////////
+/////// COUNTER
+
+
+
+
+var counted = 0;
+$(window).scroll(function () {
+
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    if (counted == 0 && $(window).scrollTop() > oTop) {
+        $('.count').each(function () {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum: countTo
+            },
+
+                {
+
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum);
+                        //alert('finished');
+                    }
+
+                });
+        });
+        counted = 1;
+    }
+
+});
+
